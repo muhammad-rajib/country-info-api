2df00cc708da4d261d78cb77842bb0e0d2e3a11a
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-08-08 01:22:36 +0600

    updated url.

--- info_manager/urls.py
+++ info_manager/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from info_manager import views
 
 urlpatterns = [
+    url('', views.country_info_list),
     url('create/country-info', views.create_country_info),
     url('update/country-info', views.update_country_info),
     # url('delete/country-info', views.delete_country_info),
-    url('view/country-info/list', views.country_info_list),
     url('view/country-details', views.country_details),
 ]
