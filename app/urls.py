87f7e66411641ff520f61e23f06a49995ca409c5
rajibhossain8521 <rajibhossain8521@gmail.com>
2021-08-08 13:56:55 +0600

    updated route urls.

--- app/urls.py
+++ app/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('info_manager.urls')),
+    path('api/v1/', include('info_manager.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- info_manager/urls.py
+++ info_manager/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from info_manager import views
 
 urlpatterns = [
-    url('', views.country_info_list),
+    url('country-info-list', views.country_info_list),
     url('view/country-details', views.country_details),
     url('create/country-info', views.create_country_info),
     url('update/country-info', views.update_country_info),
